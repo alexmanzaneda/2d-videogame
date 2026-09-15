7337cfda1e448cfbe8f04a97ae36e340a8078f48
saravigon <vidalgonzalezsara@gmail.com>
2026-09-15 12:57:10 +0200

    Add exercise 4

--- OpenGL_VS2015/OpenGL/04-transforms/04-transforms/Scene.cpp
+++ OpenGL_VS2015/OpenGL/04-transforms/04-transforms/Scene.cpp
@@ -39,31 +39,67 @@ void Scene::render()
 	program.setUniformMatrix4f("projection", projection);
 	program.setUniform4f("color", 1.0f, 1.0f, 1.0f, 1.0f);
 
-	modelview = glm::translate(glm::mat4(1.0f), glm::vec3(128.f, 48.f, 0.f));
+	// 1. Oscilación entre 0.0 (izquierda) y 1.0 (derecha)
+	float factor = (sin(currentTime / 1000.f) + 1.0f) / 2.0f;
+
+	// 2. Desplazamiento máximo en X (ej: 200 píxeles hacia la derecha)
+	float movimientoX = factor * 200.0f;
+
+	// 3. Escala progresiva: 1.0f cuando factor es 0, 0.25f cuando factor es 1
+	float escala = 1.0f - (0.75f * factor);
+
+	// --- PRIMER QUAD ---
+	float startX = 128.f;
+	float startY = 48.f;
+
+	// Aplicamos de ABAJO hacia ARRIBA
+
+	// C: Movemos el quad a su posición final en la pantalla + el desplazamiento animado
+	modelview = glm::translate(glm::mat4(1.0f), glm::vec3(startX + movimientoX, startY, 0.f));
+
+	// B2: Devolvemos el centro a su sitio
 	modelview = glm::translate(modelview, glm::vec3(64.f, 64.f, 0.f));
-	modelview = glm::rotate(modelview, -currentTime / 1000.f, glm::vec3(0.0f, 0.0f, 1.0f));
+	// B1: Aplicamos la escala
+	modelview = glm::scale(modelview, glm::vec3(escala, escala, 1.0f));
+	// B0: Movemos el quad para que su centro quede en (0,0)
 	modelview = glm::translate(modelview, glm::vec3(-64.f, -64.f, 0.f));
+
 	program.setUniformMatrix4f("modelview", modelview);
 	quad->render();
 
-	modelview = glm::translate(glm::mat4(1.0f), glm::vec3(384.f, 48.f, 0.f));
+	// --- SEGUNDO QUAD ---
+	startX = 384.f;
+	startY = 48.f;
+
+	modelview = glm::translate(glm::mat4(1.0f), glm::vec3(startX + movimientoX, startY, 0.f));
 	modelview = glm::translate(modelview, glm::vec3(64.f, 64.f, 0.f));
-	modelview = glm::rotate(modelview, currentTime / 1000.f, glm::vec3(0.0f, 0.0f, 1.0f));
+	modelview = glm::scale(modelview, glm::vec3(escala, escala, 1.0f));
 	modelview = glm::translate(modelview, glm::vec3(-64.f, -64.f, 0.f));
+
 	program.setUniformMatrix4f("modelview", modelview);
 	quad->render();
 
-	modelview = glm::translate(glm::mat4(1.0f), glm::vec3(128.f, 304.f, 0.f));
+	// --- TERCER QUAD ---
+	startX = 128.f;
+	startY = 304.f;
+
+	modelview = glm::translate(glm::mat4(1.0f), glm::vec3(startX + movimientoX, startY, 0.f));
 	modelview = glm::translate(modelview, glm::vec3(64.f, 64.f, 0.f));
-	modelview = glm::rotate(modelview, currentTime / 1000.f, glm::vec3(0.0f, 0.0f, 1.0f));
+	modelview = glm::scale(modelview, glm::vec3(escala, escala, 1.0f));
 	modelview = glm::translate(modelview, glm::vec3(-64.f, -64.f, 0.f));
+
 	program.setUniformMatrix4f("modelview", modelview);
 	quad->render();
 
-	modelview = glm::translate(glm::mat4(1.0f), glm::vec3(384.f, 304.f, 0.f));
+	// --- CUARTO QUAD ---
+	startX = 384.f;
+	startY = 304.f;
+
+	modelview = glm::translate(glm::mat4(1.0f), glm::vec3(startX + movimientoX, startY, 0.f));
 	modelview = glm::translate(modelview, glm::vec3(64.f, 64.f, 0.f));
-	modelview = glm::rotate(modelview, -currentTime / 1000.f, glm::vec3(0.0f, 0.0f, 1.0f));
+	modelview = glm::scale(modelview, glm::vec3(escala, escala, 1.0f));
 	modelview = glm::translate(modelview, glm::vec3(-64.f, -64.f, 0.f));
+
 	program.setUniformMatrix4f("modelview", modelview);
 	quad->render();
 }
